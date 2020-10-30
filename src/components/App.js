3a95a8e1c29b449ccd1b4aa18d7d236a2f2c2029
mayank1503 <mayankmotwani1503@gmail.com>
2020-10-30 09:06:43 +0530

    final test

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,25 @@
-import React, {Component, useState} from "react";
+import React, { Component } from 'react';
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            time:'',
-            interval:''
-        }
-        this.handleTime=this.handleTime.bind(this);
-    }
-    componentDidMount(){
-     this.setState(
-        { time: new Date().toLocaleTimeString(),interval:setInterval(() => {
-            
-        this.handleTime();}, 1000)}
-     )
-    }
-    handleTime(){
-    this.setState({ time: new Date().toLocaleTimeString() })
-    }
-    componentWillUnmount(){
-      clearInterval(this.state.interval);
-    }
-    render() {
-
-        return(
-            <>
-               <div> <h3 id>="time"{this.state.time}</h3></div>
-            </>
-        )
-    }
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {time: new Date().toLocaleTimeString()};
+  }
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    )
+  }
+  tick() {
+    this.setState({time: new Date().toLocaleTimeString()});
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    return (
+        <div className='Clock'> <h3 id='time'>{this.state.time}</h3> </div>
+    )
+  }
 }
-
-
-export default App;
