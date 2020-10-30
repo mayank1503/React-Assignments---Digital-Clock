c52fa8869b03d8505dac6290edf7421a8eddc4da
mayank1503 <mayankmotwani1503@gmail.com>
2020-10-30 08:41:36 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,32 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            time:'',
+            interval:''
+        }
+        this.handleTime=this.handleTime.bind(this);
+    }
+    componentDidMount(){
+     this.setState(
+        { time: (new Date).toLocaleTimeString(),interval:setInterval(() => {
+            
+        this.handleTime}, 1000)}
+     )
+    }
+    handleTime(){
+    this.setState({ time: (new Date).toLocaleTimeString() })
+    }
+    componentWillUnmount(){
+      clearInterval(this.state.interval);
+    }
     render() {
 
         return(
             <>
-               
+               <div>h3 id="time"{this.state.time}</div>
             </>
         )
     }
